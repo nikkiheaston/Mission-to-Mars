a190a833f059ad49ed4c0f6e04a43d587de1180f
nikkiheaston <nikkiheaston@gmail.com>
2022-06-04 15:24:53 -0700

    challenge complete

--- scraping.py
+++ scraping.py
@@ -7,9 +7,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
+    hemisphere_image_urls = mars_hemispheres(browser)
 
     # Run all scraping functions and store results in a dictionary
     data = {
@@ -17,7 +18,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        'hemispheres': hemisphere_image_urls
     }
 
     # Stop webdriver and return data
@@ -53,7 +55,7 @@ def mars_news(browser):
 
 def featured_image(browser):
     # Visit URL
-    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
     # Find and click the full image button
@@ -73,7 +75,7 @@ def featured_image(browser):
         return None
 
     # Use the base url to create an absolute url
-    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
     return img_url
 
@@ -92,6 +94,40 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def mars_hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+
+    for link in range(4):
+        
+        browser.find_by_tag('h3')[link].click()
+        
+        html = browser.html
+        hemi_enhanced_soup = soup(html, 'html.parser')
+        
+        #img_elem = hemi_enhanced_soup.select_one('ul')
+        #full_img_link = img_elem.find('a', target='_blank').get('href')
+        
+        full_img_link = hemi_enhanced_soup.find_all('a', target='_blank', href=True)[2].get('href')
+        img_url = f'https://marshemispheres.com/{full_img_link}'
+        
+        title = hemi_enhanced_soup.find('h2', class_='title').text
+        
+        hemispheres = {}
+        hemispheres['image_url'] = img_url
+        hemispheres['title'] = title
+        
+        hemisphere_image_urls.append(hemispheres)
+        
+        browser.back()
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
